368cdd458727f4707cf39d048ead23eb99cf6aff
sagun <sagun@gmail.com>
2020-09-24 22:08:14 +0530

    quizzing js file

---
+++ htmlquiz.js
@@ -0,0 +1,31 @@
+function abc(){
+    document.write(alert("QUIZ SUBMITTED"))
+}
+
+const quiz=[
+    {
+        q:'What does HTML stands for?',
+        options:['Hypertext Machine language','Hypertext and links markup language','Hypertext Markup Language','Hightext machine language'],
+        answer: 2
+    },
+    {
+        q:'How is document type initialized in HTML5.?',
+        options:['&lt;/DOCTYPE HTML&gt;','&lt;/DOCTYPE&gt;','&lt;!DOCTYPE HTML&gt;','&lt;/DOCTYPE html&gt;'],
+        answer: 2
+    },
+    {
+        q:' What is the font-size of the h1 heading tag?',
+        options:['3.5em','2.17em','2em','1.5em'],
+        answer: 2
+    },
+    {
+        q:'How many heading tags are there in HTML5?',
+        options:['2','3','5','6'],
+        answer: 3
+    },
+    {
+        q:'Which of the following attributes is used to add link to any element?',
+        options:['link','ref','href','newref'],
+        answer:2
+    }
+]
